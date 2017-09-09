c21b062d5157e49adac35763073a89158fdb3f61
Mohammad <mir.moh.musavi@gmail.com>
2017-09-09 16:40:45 +0430

    title

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>EOT</title>
+    <title>مجله هیجان اندیشه</title>
 
     <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}" type="text/css">
