7ff6005127f3f2a4082fb5969b22aebbca096f8f
Amin <aminsignature@yahoo.com>
2017-09-13 20:17:04 +0330

    paper details

--- resources/views/paper.blade.php
+++ resources/views/paper.blade.php
@@ -2,16 +2,31 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
             <div class="panel panel-default">
                     <div class="panel-heading">{{$papers->title}}</div>
                 <div class="panel-body">
-                    <h5>نویسندگان</h5>
-                        <p>&nbsp;{{$papers->authors_order}}</p>
+                    <div class="row">
+                   <div class="col-md-4">
+                       <h5>نویسندگان</h5>
+                    @for($i=0;$i<count($arr_name);++$i)
+                        <p>&nbsp;{{$arr_name[$i]}}&nbsp;&nbsp;({{$arr_affiliation[$i]}})&nbsp;&nbsp;&nbsp;&nbsp;{{$arr_email[$i]}}</p>
+                    @endfor
+                   </div>
+                    <div class="col-md-2">
+                    <h5>کلمات کلیدی</h5>
+                    @foreach($arr_keyword as $arr)
+                        <t>{{$arr}}&nbsp;&nbsp;,</t>
+                    @endforeach
+                    </div>
+                    <div class="col-md-1">
+                        <h5>تعداد بازدید</h5>
+                        <p>{{$views}}</p>
+                    </div>
+                </div>
+                    <div class="row container">
                     <h5>خلاصه مقاله</h5>
                         <p>&nbsp;{{$papers->abstract}}</p>
-                </div>
+                    </div>
             </div>
-        </div>
     </div>
 @endsection
